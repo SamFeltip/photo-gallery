ad4137a671e72c2dcce9dc1f2bc477ae142a2680
Sam Felton <sf.samfelton@icloud.com>
2025-09-18 16:45:03 +0100

    updated apple version and turned on vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://photos.samfelton.com",
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+    imageService: true,
+  }),
   devToolbar: {
     enabled: false,
   },
