85b894d4218ec3672078144f393fb49531e341c9
Sam Felton <sf.samfelton@icloud.com>
2025-09-26 16:42:04 +0100

    replaced git images with cloudinary

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
+import path from "path";
+import { imageService } from "@unpic/astro/service";
 
 export default defineConfig({
   site: "https://photos.samfelton.com",
   output: "server",
+  image: {
+    domains: ["res.cloudinary.com"],
+    layout: "constrained",
+    service: imageService({
+      fallbackService: "cloudinary",
+      placeholder: "blurhash",
+      layout: "constrained",
+    }),
+  },
   adapter: vercel({
     webAnalytics: {
       enabled: true,
@@ -18,6 +29,11 @@ export default defineConfig({
   integrations: [sitemap()],
   prefetch: true,
   vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
     ssr: {
       noExternal: ["smartypants"],
     },
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,76 +1,100 @@
 // src/content/config.ts
 import { defineCollection, reference, z } from "astro:content";
+import { cldAssetsLoader } from "astro-cloudinary/loaders";
 
-const imagesCollections = import.meta.glob<{ default: ImageMetadata }>(
-  "../images/collections/**/*.{jpg,jpeg,png,webp}"
-);
+import { file } from "astro/loaders";
 
-const galleryImages = import.meta.glob<{ default: ImageMetadata }>(
-  "../images/preview/**/*.{jpg,jpeg,png,webp}"
-);
+// const imagesCollections = import.meta.glob<{ default: ImageMetadata }>(
+//   "@/images/collections/**/*.{jpg,jpeg,png,webp}"
+// );
 
-const imageZod = z.object({
-  src: z.string(),
-  width: z.number(),
-  height: z.number(),
-  format: z.enum(["jpg", "jpeg", "png", "webp"]),
-  orientation: z.number().optional(),
-});
+// const galleryImages = import.meta.glob<{ default: ImageMetadata }>(
+//   "@/images/preview/**/*.{jpg,jpeg,png,webp}"
+// );
 
-const galleryCollections = defineCollection({
-  schema: z.object({
-    id: z.string(),
-    collection: reference("galleries"),
-    img: imageZod,
+// const imageZod = z.object({
+//   src: z.string(),
+//   width: z.number(),
+//   height: z.number(),
+//   format: z.enum(["jpg", "jpeg", "png", "webp"]),
+//   orientation: z.number().optional(),
+// });
+
+const cloudinaryImages = defineCollection({
+  loader: cldAssetsLoader({
+    folder: "photo-gallery/devon",
+    limit: 100,
   }),
-  loader: async () => {
-    return Promise.all(
-      Object.entries(imagesCollections).map(async ([path, image]) => {
-        const { default: imageData } = await image();
-        const galleryCollection = path
-          .replace("../images/collections/", "")
-          .replace(/\/[^/]+\.(jpg|jpeg|png|webp)/i, "");
-
-        const id = path
-          .replace(`../images/collections/${galleryCollection}/`, "")
-          .replace(/\.(jpg|jpeg|png|webp)/i, "");
-
-        return {
-          id: id,
-          collection: galleryCollection,
-          img: imageData,
-        };
-      })
-    );
-  },
 });
+
+// const galleryCollections = defineCollection({
+//   schema: ({ image }) =>
+//     z.object({
+//       id: z.string(),
+//       collection: reference("galleries"),
+//       img: imageZod,
+//     }),
+//   loader: async () => {
+//     return Promise.all(
+//       Object.entries(imagesCollections).map(async ([path, image]) => {
+//         const { default: imageData } = await image();
+//         const galleryCollection = path
+//           .replace("@/images/collections/", "")
+//           .replace(/\/[^/]+\.(jpg|jpeg|png|webp)/i, "");
+
+//         const id = path
+//           .replace(`@/images/collections/${galleryCollection}/`, "")
+//           .replace(/\.(jpg|jpeg|png|webp)/i, "");
+
+//         return {
+//           id: id,
+//           collection: galleryCollection,
+//           img: imageData,
+//         };
+//       })
+//     );
+//   },
+// });
+
+// const galleries = defineCollection({
+//   schema: ({ image }) =>
+//     z.object({
+//       name: z.string(),
+//       previewImg: imageZod,
+//       link: z.string(),
+//     }),
+//   loader: async () => {
+//     return Promise.all(
+//       Object.entries(galleryImages).map(async ([path, image]) => {
+//         const { default: imageData } = await image();
+//         const match = path
+//           .replace("@/images/preview/", "")
+//           .replace(/\/card.(jpg|jpeg|png|webp)/, "");
+
+//         return {
+//           id: match,
+//           name: match,
+//           link: `/${match}`,
+//           previewImg: imageData,
+//         };
+//       })
+//     );
+//   },
+// });
 
 const galleries = defineCollection({
-  schema: z.object({
-    name: z.string(),
-    previewImg: imageZod,
-    link: z.string(),
-  }),
-  loader: async () => {
-    return Promise.all(
-      Object.entries(galleryImages).map(async ([path, image]) => {
-        const { default: imageData } = await image();
-        const match = path
-          .replace("../images/preview/", "")
-          .replace(/\/card.(jpg|jpeg|png|webp)/, "");
-
-        return {
-          id: match,
-          name: match,
-          link: `/${match}`,
-          previewImg: imageData,
-        };
-      })
-    );
-  },
+  loader: file("src/content/galleries.json"),
+  schema: ({ image }) =>
+    z.object({
+      id: z.string(),
+      name: z.string(),
+      previewImg: z.string(),
+      link: z.string(),
+    }),
 });
 
 export const collections = {
-  galleryCollections: galleryCollections,
+  // galleryCollections: galleryCollections,
   galleries: galleries,
+  cloudinaryImages: cloudinaryImages,
 };
