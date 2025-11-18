1860b66454af9802af6e7dd228ebbbae7ff06205
Sam Felton <sf.samfelton@icloud.com>
2025-11-18 19:59:29 +0000

    added logs

--- src/lib/getBlurhash.ts
+++ src/lib/getBlurhash.ts
@@ -4,17 +4,24 @@ import { encode } from "blurhash";
 import { getPixels } from "@unpic/pixels";
 
 export async function getBlurhash(src: string): Promise<string> {
+  console.log("Generating blurhash for:", src);
   const transformedImg = transform(src, {
     width: 100,
     quality: 70,
     format: "png",
   });
+  console.log("Transformed image URL:", transformedImg);
 
   const jpgData = await getPixels(
     "https://photos.samfelton.com" + transformedImg
   );
+
   const data = Uint8ClampedArray.from(jpgData.data);
   const blurhash = encode(data, jpgData.width, jpgData.height, 4, 4);
+  console.log("Generated blurhash:", blurhash);
 
-  return blurhashToCssGradientString(blurhash);
+  const cssgradient = blurhashToCssGradientString(blurhash);
+  console.log("CSS Gradient generated!\n=======");
+
+  return cssgradient;
 }
